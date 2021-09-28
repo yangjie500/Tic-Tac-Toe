5215fba5375c30b238a6e723ce347a998b581f77
yangjie500 <kristeh97@gmail.com>
2021-09-28 21:10:04 +0800

    Finished PreGame Module, Started on Gamelogic

--- logic.js
+++ logic.js
@@ -1,4 +1,6 @@
 const preGameModal = (() => {
+    const modal = document.querySelector('.pre-game-modal');
+    const board = document.querySelector('main');
     const selectPlayer = document.querySelector('.player2');
     const selectBot = document.querySelector('.computer');
     const selectReturn = document.querySelectorAll('.return');
@@ -6,9 +8,10 @@ const preGameModal = (() => {
     const startGameBtn = document.querySelector('.start-btn');
     const playerOneInput = document.querySelector(`input[data-player='1']`);
     const playerTwoInput = document.querySelector(`input[data-player='2']`);
-    const computerInput = document.querySelectorAll(`input[data-computer]`);
     let choosePlayer;
     let computerDifficulty;
+    let haveSelected = false;
+    const playerSelected = {}
 
     const _toggleClose = () => {
         choosingDiv.classList.toggle('close');
@@ -26,12 +29,14 @@ const preGameModal = (() => {
     selectPlayer.addEventListener('click', () => {
         _toggleClose();
         _toggleOpen('player2-choosen');
+        haveSelected = true;
         choosePlayer = true;
     });
 
     selectBot.addEventListener('click', () => {
         _toggleClose();
         _toggleOpen('computer-choosen');
+        haveSelected = true;
     });
 
     selectReturn.forEach((elem) => {
@@ -46,24 +51,43 @@ const preGameModal = (() => {
     })
 
     startGameBtn.addEventListener('click', (e) => {
-        player
+        if (!haveSelected) {
+            alert('Choose an Opponent!');
+            return;
+        }
+
+        playerOne = player(playerOneInput.value);
+        playerSelected['playerOne'] = playerOne;
+
         if (choosePlayer) {
             console.log('PlayerChoosen');
+            playerTwo = player(playerTwoInput.value, display='2');
+            playerSelected['playerTwo'] = playerTwo;
         } else {
             console.log('ComputerChoosen');
             computerDifficulty = document.querySelector('input[name="difficulty"]:checked').id;
             console.log(computerDifficulty);
+            computer = computerAi(computerDifficulty);
+            playerSelected['computer'] = computer;
         }
-    })
-
-
-    return {
         
-    };
+        modal.style.display = 'none';
+        board.style.display = 'block';
+    })
+    return playerSelected;
 })();
 
-const player = (() => {
+const player = ((name, display='1') => {
+    return {
+        name,
+        display
+    };
+})
 
+const computerAi = ((difficulty, display='2') => {
+    return {
+        display
+    }
 })
 
 const gameBoard = (() => {
@@ -71,6 +95,47 @@ const gameBoard = (() => {
                     [0, 0, 0],
                     [0, 0, 0]]
 
+    const getBoard = () => _board
+
+    const updateBoard = () => {
+
+    }
+    return {
+        getBoard
+    }
 
 })();
+
+const game = (() => {
+    const grids = document.querySelectorAll('.grid-child');
+    let displayClass = 'choosenX';
+    let gameRound = 1;
+    const temp = {gameRounds: 1};
+    let gridSelected = [];
+
+    const _nextRound = () => {
+        gameRound++;
+        temp['gameRounds']++;
+    }
+    const _switchTurn = () => {
+        _nextRound();
+        if (gameRound % 2 === 1) {
+            displayClass = 'choosenX';
+        } else {
+            displayClass = 'choosenO';
+        }
+    }
+
+    grids.forEach( elem => {
+        elem.addEventListener('click', e => {
+            e.target.classList.toggle(displayClass);
+            gridSelected.push(e.target.getAttribute('data-grid'));
+            _switchTurn();
+        }, {once: true});
+    });
+
+    return {
+        gridSelected
+    }
+})()
 
